91f786f88c9cfa2a22db9d21b6a21ec541ef60f9
f1a3h <ihlwer@outlook.com>
2025-05-21 17:46:21 +0800

    overwrite upstream param

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "f1a3h/f1bsh:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
